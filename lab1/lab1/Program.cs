e268afdbbc6972852294f40bf9a985b3893c3478
s28312 <s28312@pjwstk.edu.pl>
2024-03-17 10:41:34 +0100

    smt

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -6,10 +6,34 @@ namespace lab1
     {
         public static void Main(string[] args)
         {
+            Program program = new Program();
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+
             Console.Write("mod1\n");
             Console.Write("mod2.\n");
             Console.Write("mod3.\n");
+            
+            program.sumAverageElements(arr);
 
+        }
+
+        public void sumAverageElements(int[] arr)
+        {
+            int size = arr.Length;
+            int sum = 0;
+            int avg = 0;
+
+            for (int j = 0; j < size; j++)
+            {
+                sum += arr[j];
+            }
+
+            avg = sum / size;
+
+            Console.WriteLine("The Sum 0f array: " + sum);
+            Console.WriteLine("The Average 0f array: " + avg);
         }
     }
 }
