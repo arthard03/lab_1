243e9bb336cef303fe9cc21d205e49cef8a02424
s28312 <s28312@pjwstk.edu.pl>
2024-03-17 09:54:49 +0100

    Added modification1

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("mod1\n");
+            Console.Write("mod2\n");
+            Console.Write("mod3\n");
+
+        }
+    }
+}
