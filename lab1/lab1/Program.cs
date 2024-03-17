f176c7966ee5eb0a030957d30ae612b8f9caee30
s28312 <s28312@pjwstk.edu.pl>
2024-03-17 10:05:27 +0100

    Task 3) method that calculates average from array of ints

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -6,10 +6,34 @@ namespace lab1
     {
         public static void Main(string[] args)
         {
+            Program program = new Program();
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+
             Console.Write("mod1\n");
             Console.Write("mod2.\n");
             Console.Write("mod3.\n");
+            
+            program.sumAverageElements(arr);
 
+        }
+
+        public void sumAverageElements(int[] arr)
+        {
+            int size = arr.Length;
+            int sum = 0;
+            int average = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+
+            average = sum / size;
+
+            Console.WriteLine("The Sum 0f array: " + sum);
+            Console.WriteLine("The Average 0f array: " + average);
         }
     }
 }
